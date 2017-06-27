56e985f336cd652596dd96a274d42d4dab85418c
Kachorroski <kernelkill@outlook.com>
2017-06-27 19:38:36 -0400

    Mudando estrutura de diretorio

---
+++ src/br/com/estruturadados/loiane/app/AppU.java
@@ -0,0 +1,23 @@
+package br.com.estruturadados.loiane.app;
+
+import br.com.estruturadados.loiane.classes.Vetor;
+
+public class AppU {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vetor vetor = new Vetor(3);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+		vetor.adiciona("elemento 4");
+		vetor.adiciona("elemento 5");
+		vetor.adiciona("elemento 6");
+		
+		System.out.println(vetor);
+
+	}
+
+}
